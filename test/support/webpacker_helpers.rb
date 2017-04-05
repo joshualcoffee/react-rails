62d9f7b718ede34da188418e155cfcf2c4004f28
Robert Mosolgo <rdmosolgo@gmail.com>
2017-04-05 11:38:59 -0400

    feat(WebpackerManifestContainer) use webpack-dev-server if detected

--- test/support/webpacker_helpers.rb
+++ test/support/webpacker_helpers.rb
@@ -3,14 +3,17 @@ module WebpackerHelpers
 
   def when_webpacker_available
     if defined?(Webpacker)
-      clear_webpacker_packs
-      Dir.chdir("./test/dummy") do
-        Rake::Task['webpacker:compile'].invoke
-      end
-      # Reload cached JSON manifest:
-      Webpacker::Manifest.load
       yield
     end
+  end
+
+  def compile
+    clear_webpacker_packs
+    Dir.chdir("./test/dummy") do
+      Rake::Task['webpacker:compile'].invoke
+    end
+    # Reload cached JSON manifest:
+    Webpacker::Manifest.load
   end
 
   def clear_webpacker_packs
